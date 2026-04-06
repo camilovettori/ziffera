ddb85fc8c6347437e92fe34408f56275211c6317
Camilo <camilovettori@gmail.com>
2026-04-06 18:08:12 +0100

    imigration

--- migrations/0005_email_events_updated_at.sql
+++ migrations/0005_email_events_updated_at.sql
@@ -3,8 +3,4 @@ BEGIN;
 ALTER TABLE email_events
 ADD COLUMN IF NOT EXISTS updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
 
-UPDATE email_events
-SET updated_at = COALESCE(updated_at, queued_at, sent_at, failed_at, created_at, NOW())
-WHERE updated_at IS NULL;
-
 COMMIT;
